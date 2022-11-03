929dbbaa1ac609d206da2e437861fd8f873d70b4
Krost22 <eduardomogollonsalcedo@gmail.com>
2022-11-03 16:25:33 -0500

    3/11/2022
    Arreglé el sistema de inventario, agregue a "personaje 2 " , añadí animaciones al personaje (caminar y Idle)

--- Assets/Custom/Scripts/PlayerMovement.cs
+++ Assets/Custom/Scripts/PlayerMovement.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     NavMeshAgent agent;
 
+    Rigidbody rigidbody; //con el rigid body podemos saber la velocidad del personaje
+
+    Animator animator;
+    const string STATE_QUIETO = "Quieto";
+
     // Start is called before the first frame update
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        //Aqui importamos el parametro animator para controlar las animaciones
+        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (agent.velocity.magnitude <= 0)
+        {
+            animator.SetBool(STATE_QUIETO, true);
+        }
+
         if (Input.GetMouseButton(0))
         {
+
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out hit, Mathf.Infinity)) //Distancia maxima del rayo en este caso es infinita
             {
                 agent.SetDestination(hit.point);
-                
-                
+
+                animator.SetBool(STATE_QUIETO, false);
+
             }
         }
     }
--- README.md
+++
@@ -1,1 +0,0 @@
-# Proyecto_de_grado_videojuego
