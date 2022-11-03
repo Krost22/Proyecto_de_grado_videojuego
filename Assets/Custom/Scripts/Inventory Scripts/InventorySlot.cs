01c25f784c0e36dff320faf650ed39ef9ff11aca
Krost22 <eduardomogollonsalcedo@gmail.com>
2022-11-03 17:01:53 -0500

    Revert "3/11/2022"
    This reverts commit 929dbbaa1ac609d206da2e437861fd8f873d70b4.

--- Assets/Custom/Scripts/Inventory Scripts/InventorySlot.cs
+++ Assets/Custom/Scripts/Inventory Scripts/InventorySlot.cs
@@ -28,12 +28,6 @@ public class InventorySlot
         stackSize = -1;
     }
 
-    public void UpdateInventorySlot(InventoryItemData data, int amount)
-    {
-        itemData = data;
-        stackSize = amount;
-    }
-
     public bool RoomLeftInStack(int amountToAdd, out int amountRemaining)
     {
         amountRemaining = ItemData.MaxStackSize - stackSize;
